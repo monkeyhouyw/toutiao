8b2821b29febaa5bf7f634370e8e66ac2f9983ed
侯亚文 <1269039291@qq.com>
2019-12-03 23:03:00 +0800

    使用vant

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入vant
+import Vant from 'vant'
+// 导入vant样式
+import 'vant/lib/index.css'
+
+// 注册vant,可知vant十个插件，里面包含很多组件，是组件库
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
